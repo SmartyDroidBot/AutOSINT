aa4e811686c0e25ef60edcd75f0f58fdd7edd70d
SmartyDroiBot <eshaanbhatu@gmail.com>
2024-03-02 18:42:15 +0530

    added meths

--- app.py
+++ app.py
@@ -1,11 +1,13 @@
 from flask import Flask, render_template, url_for, request
 
+from meths.meth import geoip
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     if request.method=="POST":
         query=request.form['query']
+
     else:
         return render_template('index.html')
 
