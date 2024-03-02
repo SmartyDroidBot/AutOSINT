70af1b90141474924f7c8af74e92d96fc9b9e6bc
NITIN PRAJWAL R <118678001+nitinprajwal@users.noreply.github.com>
2024-03-02 17:02:58 +0530

    Add files via upload

---
+++ IP-OSINT/ipsint.py
@@ -0,0 +1,38 @@
+import requests
+import json
+
+# Beautify the top
+def banner():
+    print("""\033[30;46m Simple IP OSINT (Open Source Intelligence) \033[0m\n""")
+banner()
+
+# Ask the IP that you want to extract
+ip = input("\033[30;43m Enter the Target IP \033[0m : ")
+
+# We use the site ipinfo.io
+url = "https://ipinfo.io/"+ip+"/json"
+response = requests.get(url)
+
+# If the generated status code is 200, the extraction was successful
+if response.status_code == 200:
+    result = response.json()
+    
+    # Create a dictionary with the required information
+    output = {
+        "IP": result.get("ip"),
+        "Hostname": result.get("hostname"),
+        "Anycast": result.get("anycast"),
+        "City": result.get("city"),
+        "Region": result.get("region"),
+        "Country": result.get("country"),
+        "Location": result.get("loc"),
+        "Organization": result.get("org"),
+        "ZIP/Postal Code": result.get("postal"),
+        "Time Zone": result.get("timezone")
+    }
+    
+    # Print the output dictionary
+    print(json.dumps(output, indent=4))
+else:
+    # Unless the status code is 200, the extraction failed
+    print("There is something wrong! The web is unreachable. HTTP Response:", response.status_code)
