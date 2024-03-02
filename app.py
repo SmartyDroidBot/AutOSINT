41059010274f302189b50ef75f3a6597baa954c6
OkayStark <psaurav76@gmail.com>
2024-03-02 18:10:54 +0530

    fixed index and added reports

--- app.py
+++ app.py
@@ -3,9 +3,12 @@ from flask import Flask, render_template, url_for
 app = Flask(__name__)
 
 @app.route('/')
-
 def index():
     return render_template('index.html')
 
-if __name__=="__main__":
+@app.route('/reports')
+def reports():
+    return render_template('reports.html')
+
+if __name__ == "__main__":
     app.run(debug=True)
